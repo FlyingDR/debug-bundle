b4b9900963523bf245c2b3b9dc5fc5127cf0bb93
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2014-06-21 16:02:23 +0400

    Registration of debug security factory is moved into own bundle class

--- DebugBundle.php
+++ DebugBundle.php
@@ -5,6 +5,8 @@ namespace Flying\Bundle\DebugBundle;
 use Flying\Bundle\DebugBundle\DependencyInjection\Compiler\CsrfTokenManagerSubstitutionPass;
 use Flying\Bundle\DebugBundle\DependencyInjection\Compiler\RegisterDebuggerStatusSubscribersPass;
 use Flying\Bundle\DebugBundle\DependencyInjection\Compiler\SecurityServicesConfigurationPass;
+use Flying\Bundle\DebugBundle\Security\Factory\DebugAuthenticationFactory;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -19,5 +21,8 @@ class DebugBundle extends Bundle
         $container->addCompilerPass(new SecurityServicesConfigurationPass());
         $container->addCompilerPass(new CsrfTokenManagerSubstitutionPass());
         $container->addCompilerPass(new RegisterDebuggerStatusSubscribersPass());
+        /** @var $security SecurityExtension */
+        $security = $container->getExtension('security');
+        $security->addSecurityListenerFactory(new DebugAuthenticationFactory());
     }
 }
